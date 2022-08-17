b6023a4f1beca0300f8a52e04026962941fc0eb3
bendevman <bendevman@gmail.com>
2022-08-17 14:34:49 +0300

    css splitChunks fix

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,5 +1,7 @@
 import 'normalize.css';
 import '../../fonts/fonts.scss';
+import '../../styles/reset.scss';
+import '../../styles/main.scss';
 
 import '../../components/header/header';
 import '../../components/footer/footer';
